b93e4cf947f22f690798c0f0896adfcbe5976da4
andreanatile <andrea.natile.an@gmail.com>
2025-12-10 12:30:12 +0100

    add rt-detr constants model

--- src/models/constants.py
+++ src/models/constants.py
@@ -2,9 +2,15 @@ DETECTION = "detection"
 SEGMENTATION = "segmentation"
 
 YOLO_MODELS = {
-    "YOLOV11-N":"checkpoints/yolo11n.pt",
-    "YOLOV11-S":"checkpoints/yolo11s.pt",
-    "YOLOV11-M":"checkpoints/yolo11m.pt",
-    "YOLOV11-L":"checkpoints/yolo11l.pt",
-    "YOLOV11-X":"checkpoints/yolo11x.pt",
+    "YOLOV11-N": "checkpoints/yolo11n.pt",
+    "YOLOV11-S": "checkpoints/yolo11s.pt",
+    "YOLOV11-M": "checkpoints/yolo11m.pt",
+    "YOLOV11-L": "checkpoints/yolo11l.pt",
+    "YOLOV11-X": "checkpoints/yolo11x.pt",
+}
+RT_DETR_MODELS = {
+    "RT-DETRv2-S": "PekingU/rtdetr_v2_r18vd",
+    "RT-DETRv2-M": "PekingU/rtdetr_v2_r34vd",
+    "RT-DETRv2-L": "PekingU/rtdetr_v2_r50vd",
+    "RT-DETRv2-x": "PekingU/rtdetr_v2_r101vd",
 }
